dac30ace922611b7586fbfce39c9e714fbc6f8d9
markvnr <62091480+markvnr@users.noreply.github.com>
2020-03-12 10:23:57 +0200

    first commit of lab 3 and 4

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentData
+{
+    class Program
+    {
+
+       public static void Main(string[] args)
+        {
+            int i = 1;
+            List<Student> students = new List<Student>();
+            Student student = new Student();
+
+            Console.WriteLine("Write Student name:");
+            student.SetName(Console.ReadLine());
+
+            Console.WriteLine("Write Student surname:");
+            student.SetSurname(Console.ReadLine());
+
+            Console.WriteLine("Write Student exam result:");
+            student.SetExamResult(int.Parse(Console.ReadLine()));
+
+            while (true) {
+                Console.WriteLine("Write Student result for homework {0}:",i);
+                student.AddHomeworkResult(int.Parse(Console.ReadLine()));
+                students.Add(student);
+                Console.WriteLine("more grades? y/n \n");
+                char ch = Console.ReadKey().KeyChar;
+                if (ch == 'n')
+                {
+                    break;
+                }
+                else {
+                    i++;
+                    Console.WriteLine();
+                }
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("{0,-10} {1,-10} {2,20}", "Name", "Surname","Final Points (Avg.)");
+            Console.WriteLine("-------------------------------------------------------------");
+            double result = 0.7 * student.GetExamResult() + 0.3 * student.CalculateFinalPointsAverage();
+            Console.WriteLine("{0,-10} {1,-10} {2,20}",student.GetName(),student.GetSurname(),result);
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentData
+{
+    class Student
+    {
+        private string name;
+        private string surname;
+        private int examResult;
+        private readonly List<int> homeworkResults;
+
+        public Student(string name,string surname) {
+            this.name = name;
+            this.surname = surname;
+            this.examResult = -1;
+            this.homeworkResults = new List<int>();
+        }
+
+        public Student() {
+            this.examResult = -1;
+            this.homeworkResults = new List<int>();
+        }
+
+        public void SetName(string name) {
+            this.name = name;
+        }
+
+        public void SetSurname(string surname)
+        {
+            this.surname = surname;
+        }
+
+        public void SetExamResult(int examResult)
+        {
+            this.examResult = examResult;
+        }
+
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        public string GetSurname()
+        {
+            return this.surname;
+        }
+
+        public int GetExamResult()
+        {
+            return this.examResult;
+        }
+
+        public void AddHomeworkResult(int result) {
+            this.homeworkResults.Add(result);
+        }
+
+        public double CalculateFinalPointsAverage() {
+            int sum = 0;
+            foreach (int value in this.homeworkResults)
+            {
+                sum += value;
+            }
+
+            double avg = (double)sum / this.homeworkResults.Count;
+            return 0.7 * this.examResult + 0.3 * avg;
+        }
+    }
+}
